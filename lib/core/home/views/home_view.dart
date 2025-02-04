feb37316b3505f311933a1e2f9c1f0d8610dfa2e
zero <himura.js@hotmail.com>
2025-02-04 10:47:12 -0500

    feat: HomeDrawer added

---
+++ lib/core/home/views/home_view.dart
@@ -0,0 +1,28 @@
+import 'package:byte/core/home/widgets/home_drawer.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Builder(
+          builder: (context) => IconButton(
+            onPressed: () => Scaffold.of(context).openDrawer(),
+            icon: Icon(Icons.menu_rounded),
+          ),
+        ),
+        title: Text('Byte App'),
+        actions: [
+          IconButton(
+            onPressed: null,
+            icon: Icon(Icons.logout_rounded),
+          ),
+        ],
+      ),
+      drawer: HomeDrawer(),
+    );
+  }
+}
---
+++ lib/core/home/widgets/home_drawer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeDrawer extends StatelessWidget {
+  const HomeDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.lightBlue,
+            ),
+            child: Text('Drawe header'),
+          ),
+        ],
+      ),
+    );
+  }
+}
