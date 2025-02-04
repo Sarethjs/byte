13850e385f8a35de714fcac98b9f0f101e43b7e7
zero <himura.js@hotmail.com>
2025-02-04 11:24:40 -0500

    feat: color theme added

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,13 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.black,
+          brightness: Brightness.light,
+        ),
+      ),
       debugShowCheckedModeBanner: false,
       home: HomeView(),
     );
